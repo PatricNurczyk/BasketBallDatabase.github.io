e5cc3a489f8795fd5466c9ef90e90c7aa900c738
PatricNurczyk <115892428+PatricNurczyk@users.noreply.github.com>
2023-04-05 15:49:41 -0400

    Update MainWebController.py

--- MainWebController.py
+++ MainWebController.py
@@ -3,10 +3,11 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_USER'] = 'webapp'
+app.config['MYSQL_PASSWORD'] = 'webapp'
 app.config['MYSQL_DB'] = 'flask'
 mysql = MySQL(app)
+
 
 
 @app.route('/', methods=['GET'])
@@ -16,7 +17,11 @@ def home():
 
 @app.route('/testpage', methods=['GET'])
 def test():
-    return "THIS IS ANOTHER PAGE"
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM useraccounts")
+    rv =  cursor.fetchall()
+    cursor.close()
+    return rv
 
 
 if __name__ == '__main__':
